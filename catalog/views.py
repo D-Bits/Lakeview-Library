a25fc075c4eb25951f0d287fae201f493e8a31f1
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-27 15:47:13 -0700

    Added tempaltes and index view.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from . models import Book, BookInstance, Author, Genre
+
 
 # Create your views here.
+def index(request):
+
+    # Generate number of books available
+    num_books = Book.objects.all().count()
+    # Generate number of instances of books
+    num_instances = BookInstance.objects.all().count()
+
+    # Available copies (status='a')
+    instances_available = BookInstance.objects.filter(status_exact='a').count()
+
+    # Count the number of authors
+    num_authors = Author.objects.all()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
--- lakeview_library/urls.py
+++ lakeview_library/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/')),
+   # path('catalog/', include('catalog.urls')),
+   # path('', RedirectView.as_view(url='/catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
